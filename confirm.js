563bcd2c46ab89c65522a2e72e0ff370fd480df5
Alef oliveira <you@example.com>
2020-08-21 17:22:30 -0300

    stage 2

--- confirm.js
+++
@@ -1,7 +0,0 @@
-const formDelete = document.querySlector("#form-delete")
-formDelete.addEventListner("submit" , function(e){
-const confirmation = comfirm("Deseja deletar?")
-if(!confirmation){
-    e.preventDefault()
-}
-})
